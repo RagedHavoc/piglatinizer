c6111db7922d7214be81c28d4dd93f6d21a63ab3
RagedHavoc <annie.johnson1we@gmail.com>
2019-10-30 22:25:56 +0000

    Added click handler

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+var piglatin;
+
+$("button").click(function(){
+ var piglatin = $("input").val();
+
+}
+)
 
 
-  
-  
-  
-  
+
+
+
+
+
 });
